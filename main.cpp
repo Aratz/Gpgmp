aaddc9be2cadb834fdf3bf78f74552744bb44d31
Adrien Coulier <adrien.coulier@it.uu.se>
2020-07-03 09:06:02 +0200

    Add basic test case

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <random>
 #include <vector>
 
+#include <cassert>
+#include<cmath>
+
 typedef std::vector<int> species;
 typedef std::function<double(const species&)> propensity;
 
@@ -37,5 +40,33 @@ double ssa(
 }
 
 int main() {
+    double mu = 100.0;
+    double gamma = 1.0;
+
+    double t_end = 1000.;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    species x {0};
+    std::vector<species> stoich_matrix {
+        { 1 },
+        { -1 }
+    };
+    const std::vector<propensity> propensities {
+        [mu] (const species& x) { return mu; },
+        [gamma] (const species& x) { return gamma * x[0]; },
+    };
+
+    double t = 0.0;
+    while (t < t_end) {
+        std::cout << "t:" << t << "; ";
+        std::cout << "P:" << x[0] << "\n";
+        t += ssa(x, propensities, stoich_matrix, gen);
+    }
+    std::cout << "P:" << x[0] << "\n";
+    std::cout << "t:" << t << "\n";
+
+    assert(std::abs((x[0] - mu/gamma)/(mu/gamma)) < 0.2);
     return 0;
 }
