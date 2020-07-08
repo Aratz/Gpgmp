abc48aee283cd6a1a6d9cc95b15e6c49a6aea131
Adrien Coulier <adrien.coulier@it.uu.se>
2020-07-08 11:42:48 +0200

    Test that ssa simulates the correct duration

--- tests/SsaTest.cpp
+++ tests/SsaTest.cpp
@@ -12,6 +12,7 @@ class SsaTest: public QObject {
 
         private slots :
             void testSsa();
+            void testTime();
 };
 
 //TODO test empty
@@ -41,6 +42,30 @@ void SsaTest::testSsa() {
     ssa(x, t_end, propensities, stoich_matrix, gen);
 
     QVERIFY(std::abs((x[0] - mu/gamma)/(mu/gamma)) < 0.2);
+}
+
+void SsaTest::testTime() {
+    double mu = 1.;
+    double t_end = 500.;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    int Ns = 1;
+
+    grid domain(boost::extents[1][1][Ns]);
+    species x = domain[boost::indices[0][0][range(0, Ns)]];
+
+    std::vector<std::vector<int>> stoich_matrix {
+        { 1 },
+    };
+    const std::vector<propensity> propensities {
+        [mu] (const species& x) { return mu; },
+    };
+
+    ssa(x, t_end, propensities, stoich_matrix, gen);
+
+    QVERIFY(std::abs((x[0] - t_end*mu)) < 0.2*t_end*mu);
 }
 
 QTEST_MAIN(SsaTest)
